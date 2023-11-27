476348a9db63bf722d63310903df28ec3c166478
omarfaruk70 <faruk1611281892@gmail.com>
2023-11-27 13:06:50 +0600

    added mycart delete function

--- src/components/Mycart/Mycart.jsx
+++ src/components/Mycart/Mycart.jsx
@@ -1,53 +1,42 @@
+import Swal from "sweetalert2";
 import { useLoaderData } from "react-router-dom";
 import { AiFillDelete } from "react-icons/ai";
 import { useState } from "react";
-import Swal from "sweetalert2";
 const Mycart = () => {
   const loadedData = useLoaderData();
-  const [mycart, setMycart] = useState(loadedData)
-  const  handleDeletefromMycart = (id) => {
-   fetch(`http://localhost:5000/mycart/${id}`, {
-    method: "DELETE"
-   })
-   .then(res => res.json())
-   .then(data => {
-    if(data.deletedCount > 0){
-      Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire({
-            title: "Deleted!",
-            text: "Your file has been deleted.",
-            icon: "success"
+  const [mycart, setMycart] = useState(loadedData);
+  const handleDeletefromMycart = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if(result.isConfirmed) {
+        fetch(`http://localhost:5000/mycart/${id}`, {
+          method: "DELETE",
+        }).then((res) => res.json())
+          .then((data) => {
+           if(data.deletedCount > 0){
+             const remaining = mycart.filter((cart) => cart._id !== id);
+             setMycart(remaining);
+           }
           });
-        }
-        const remaining = mycart.filter(cart => cart._id !== id);
-        setMycart(remaining)
-      });
-    }
-   })
-  }
+      }
+    });
+  };
   return (
     <div className="mx-5 my-10">
       {mycart?.length > 0 ? (
         <div className=" grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 mx-8">
           {mycart.map((data) => (
-            <div
-              key={data._id}>
+            <div key={data._id}>
               <div className="card lg:w-96 glass">
                 <figure>
-                  <img 
-                    className="w-full"
-                    src={data.img}
-                    alt="car!"
-                  />
+                  <img className="w-full" src={data.img} alt="car!" />
                 </figure>
                 <div className="card-body">
                   <h2 className="card-title">Name: {data.name}</h2>
@@ -56,7 +45,12 @@ const Mycart = () => {
                   <p>Price: {data.price} $</p>
                   <p>Ratings: {data.ratings}</p>
                   <div className="card-actions justify-center">
-                    <button onClick={() => handleDeletefromMycart(data._id)} className="btn bg-red-500 hover:bg-red-600 text-white">Delete <AiFillDelete  className="font-bold text-xl"/> </button>
+                    <button
+                      onClick={() => handleDeletefromMycart(data._id)}
+                      className="btn bg-red-500 hover:bg-red-600 text-white"
+                    >
+                      Delete <AiFillDelete className="font-bold text-xl" />{" "}
+                    </button>
                   </div>
                 </div>
               </div>
